6c6f66e9400137974b6d68e181024e46562a31ba
Karmelica <damianniedziolka7@gmail.com>
2023-11-03 14:35:11 +0100

    miecz

--- Assets/Scripts/Flipper3.cs
+++ Assets/Scripts/Flipper3.cs
@@ -4,31 +4,27 @@ public class Flipper3 : MonoBehaviour
 {
     [SerializeField]
     public SpriteRenderer spriteRenderer;
-    public Transform player;
+    public GameObject player;
 
 
-    void Flip()
-    {
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            transform.position = new Vector3(player.position.x + -0.2f, player.position.y, 0);
-            spriteRenderer.flipX = true;
-        }
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            transform.position = new Vector3(player.position.x + 0.2f, player.position.y, 0); ;
-            spriteRenderer.flipX = false;
-        }
-    }
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Flip();
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.position = new Vector3(player.transform.position.x - 0.2f, player.transform.position.y, 0);
+            spriteRenderer.flipX = true;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.position = new Vector3(player.transform.position.x + 0.2f, player.transform.position.y, 0); ;
+            spriteRenderer.flipX = false;
+        }
     }
 }
