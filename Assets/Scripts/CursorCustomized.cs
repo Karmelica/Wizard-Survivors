c593ca228d95ad75c8c99eb7d8d1483d2e0652d6
Karmelica <damianniedziolka7@gmail.com>
2023-11-09 01:41:39 +0100

    skrypt problem

--- Assets/Scripts/CursorCustomized.cs
+++ Assets/Scripts/CursorCustomized.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class CursorCustom : MonoBehaviour
+public class CursorCustomized : MonoBehaviour
 {
-    private float mousePos;
+    private Vector2 mousePos;
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Cursor.visible = false;
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
     }
 }
