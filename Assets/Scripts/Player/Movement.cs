ce3b5620d609ec00a3e38d19ef1d0ce286e6edc2
Karmelica <damianniedziolka7@gmail.com>
2024-01-16 14:42:15 +0100

    attack sounds, animations, forest

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,126 +1,140 @@
 using System.Collections;
-using System.Text;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-	private float moveX;
-	private float moveY;
+    private float moveX;
+    private float moveY;
 
-	public TrailRenderer trail;
-	private Rigidbody2D rbody;
-	public Collider2D collider2d;
-	public Animator animator;
-	static public bool dashUnlocked;
+    private Stats stats;
+    public AudioSource audioSourceLoop;
+    public TrailRenderer trail;
+    private Rigidbody2D rbody;
+    public Collider2D collider2d;
+    public Animator animator;
+    static public bool dashUnlocked;
 
     [Header("Ruch")]
-	private float dashReducer = 1f; //controls dash power
-	public float setPlayerSpeed = 5f;
-	public float setDashCooldown = 5f;
-	public static float playerSpeed;
-	static public float dashCooldown;
-	static public float uiDashCooldown;
-	static public bool isDashing = false;
+    private bool stopped;
+    private float dashReducer = 1f; //controls dash power (0 - 1)
+    public float setPlayerSpeed = 5f;
+    public float setDashCooldown = 5f;
+    public static float playerSpeed;
+    static public float dashCooldown;
+    static public float uiDashCooldown;
+    static public bool isDashing = false;
 
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		if (collision.gameObject.CompareTag("IceLake"))
-		{
-			dashReducer = 0.3f;
-			rbody.drag = 1; //friction
-		}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("IceLake"))
+        {
+            dashReducer = 0.3f;
+            rbody.drag = 1; //friction
+        }
 
-		if (collision.gameObject.CompareTag("LavaLake"))
-		{
-			dashReducer = 0.6f;
-			rbody.drag = 10;
-		}
-	}
+        if (collision.gameObject.CompareTag("LavaLake"))
+        {
+            dashReducer = 0.6f;
+            rbody.drag = 10;
+        }
+    }
 
-	private void OnTriggerExit2D(Collider2D collision)
-	{
-		if (collision.gameObject.CompareTag("IceLake"))
-		{
-			dashReducer = 1f;
-			rbody.drag = 6;
-		}
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("IceLake"))
+        {
+            dashReducer = 1f;
+            rbody.drag = 6;
+        }
 
-		if (collision.gameObject.CompareTag("LavaLake"))
-		{
-			dashReducer = 1f;
-			rbody.drag = 6;
-		}
-	}
+        if (collision.gameObject.CompareTag("LavaLake"))
+        {
+            dashReducer = 1f;
+            rbody.drag = 6;
+        }
+    }
 
-	public void IsInBorder()
-	{
-		rbody.velocity = Vector3.zero;
-	}
+    public void IsInBorder()
+    {
+        rbody.velocity = Vector2.zero;
+    }
 
-	void Flip()
-	{
-		if (moveX < 0)
-		{
-			transform.rotation = Quaternion.Euler(0, 180, 0);
-			trail.transform.position = transform.position + new Vector3(0, 0, 1f);
-		}
-		if (moveX > 0)
-		{
-			transform.rotation = Quaternion.Euler(0, 0, 0);
-			trail.transform.position = transform.position + new Vector3(0, 0, 1f);
-		}
-	}
-	IEnumerator DashHandler()
-	{
-		dashCooldown = setDashCooldown;
-		isDashing = true;
-		collider2d.isTrigger = true;
-		animator.Play("Dash");
-		trail.emitting = true;
+    void Flip()
+    {
+        if (moveX < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            trail.transform.position = transform.position + new Vector3(0, 0, 1f);
+        }
+        if (moveX > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            trail.transform.position = transform.position + new Vector3(0, 0, 1f);
+        }
+    }
+    IEnumerator DashHandler()
+    {
+        dashCooldown = setDashCooldown;
+        isDashing = true;
+        collider2d.isTrigger = true;
+        animator.Play("Dash");
+        trail.emitting = true;
 
-		rbody.AddForce(playerSpeed * dashReducer * rbody.velocity.normalized, ForceMode2D.Impulse);
+        rbody.AddForce(playerSpeed * dashReducer * rbody.velocity.normalized, ForceMode2D.Impulse);
 
-		yield return new WaitForSeconds(1.5f);
-		collider2d.isTrigger = false;
-		isDashing = false;
-		trail.emitting = false;
-	}
-	void MyInput()
-	{
-		moveX = Input.GetAxis("Horizontal");
-		moveY = Input.GetAxis("Vertical");
-	}
+        yield return new WaitForSeconds(1.5f);
+        collider2d.isTrigger = false;
+        isDashing = false;
+        trail.emitting = false;
+    }
+    void MyInput()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+    }
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		dashUnlocked = false;
-		playerSpeed = setPlayerSpeed;
-		uiDashCooldown = setDashCooldown;
-		rbody = GetComponent<Rigidbody2D>();
-		rbody.freezeRotation = true;
-		dashCooldown = 0f;
-	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSourceLoop.enabled = true;
+        dashUnlocked = false;
+        playerSpeed = setPlayerSpeed;
+        uiDashCooldown = setDashCooldown;
+        stats = GetComponent<Stats>();
+        rbody = GetComponent<Rigidbody2D>();
+        rbody.freezeRotation = true;
+        dashCooldown = 0f;
+    }
 
-	// Update is called once per frame
-	void FixedUpdate()
-	{
-		MyInput();
-		rbody.AddForce(0.5f * playerSpeed * new Vector2(moveX, moveY).normalized, ForceMode2D.Force);
-		if (rbody.velocity.magnitude * 10 > 1f && !isDashing)
-		{
-			animator.Play("walk_anim");
-		}
-	}
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MyInput();
+        rbody.AddForce(0.5f * playerSpeed * new Vector2(moveX, moveY).normalized, ForceMode2D.Force);
+        if (rbody.velocity.magnitude * 10 > 1f && !isDashing && !AnimationEvents.attackAnim)
+        {
+            animator.Play("walk_anim");
+        }
+    }
 
-	void Update()
-	{
-		Flip();
+    void Update()
+    {
+        /*if (AnimationEvents.hasStopped)
+        {
+            audioSourceLoop.enabled = false;
+            stats.PlaySoundLoop(BiomeDetector.currentWalkClip, 1);
+        }
+        else
+        {
+            audioSourceLoop.enabled = true;
+        }*/
 
-		dashCooldown -= Time.deltaTime;
-		if (Input.GetKeyDown(KeyCode.LeftShift) && dashCooldown <= 0 && dashUnlocked)
-		{
-			StartCoroutine(DashHandler());
-		}
-	}
+        Flip();
+
+        dashCooldown -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashCooldown <= 0 && dashUnlocked)
+        {
+            StartCoroutine(DashHandler());
+        }
+    }
 }
--- Assets/Scripts/Player/Stats.cs
+++ Assets/Scripts/Player/Stats.cs
@@ -1,78 +1,77 @@
 using System.Collections;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Stats : MonoBehaviour
 {
 
-	[Header("Stats")]
-	public int maxHp = 20;
-	static public int currentHp;
+    [Header("Stats")]
+    public int maxHp = 20;
+    static public int currentHp;
 
-	public static int exp = 0;
-	public int maxExp = 100;
-	public int currentExp;
-	static public int currentLevel = 1;
+    public static int exp = 0;
+    public int maxExp = 100;
+    public int currentExp;
+    static public int currentLevel = 1;
 
-	public int currentCoins;
+    public int currentCoins;
 
-	public int projectileDmg = 1;
-	public int slashDmg = 2;
-	public int dashDmg = 2;
-	public int earthDmg = 1;
+    public int projectileDmg = 1;
+    public int slashDmg = 2;
+    public int dashDmg = 2;
+    public int earthDmg = 1;
 
-	static public string biome;
-	//private float overHeal;
-	//private float vel = 1f;
+    static public string biome;
+    //private float overHeal;
+    //private float vel = 1f;
 
-	[Header("Components")]
-	public ShopUIManager shopUIManager;
-	public TextMeshProUGUI textMeshHP;
-	public HealthBar healthBar;
-	public ExpBar expBar;
-	public LevelCounter levelCounter;
-	public CoinCounter coinCounter;
-	public Image overHealBar;
-	public GameObject MagnetCollider;
-	public MagnetText magnetText;
-	public AudioClip lvlUp;
-	public AudioClip magnetUnlocked;
-	public AudioClip takeDamage;
-	[SerializeField] private AudioSource audioSource;
-	[SerializeField] private AudioSource audioSourceLoop;
+    [Header("Components")]
+    public ShopUIManager shopUIManager;
+    public TextMeshProUGUI textMeshHP;
+    public HealthBar healthBar;
+    public ExpBar expBar;
+    public LevelCounter levelCounter;
+    public CoinCounter coinCounter;
+    //public Image overHealBar;
+    public GameObject MagnetCollider;
+    public MagnetText magnetText;
+    public AudioClip lvlUp;
+    public AudioClip magnetUnlocked;
+    public AudioClip[] takeDamage;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioSource audioSourceLoop;
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "pfSnowball(Clone)")
+        if (collision.gameObject.name == "pfSnowball(Clone)" || collision.gameObject.name == "pfFireProjectile(Clone)")
         {
             TakeDamage(EnemyScript.enemyDmg);
             Destroy(collision.gameObject);
         }
     }
 
-	IEnumerator AudioLoop(AudioClip sound, float volume)
-	{
-		while (true)
-		{
-			float lenght = sound.length;
-			audioSourceLoop.PlayOneShot(sound, volume);
-			yield return new WaitForSeconds(lenght - 0.1f);
-		}
-	}
-
-	public void PlaySoundOneShot(AudioClip sound, float volume)
-	{
-        audioSource.PlayOneShot(sound, volume);
+    IEnumerator AudioLoop(AudioClip sound, float volume)
+    {
+        float lenght = sound.length;
+        while (audioSourceLoop.enabled)
+        {
+            audioSourceLoop.PlayOneShot(sound, volume);
+            yield return new WaitForSeconds(lenght - 0.1f);
+        }
     }
 
-	public void PlaySoundLoop(AudioClip sound, float volume)
+    public void PlaySoundLoop(AudioClip sound, float volume)
     {
         StopAllCoroutines();
         audioSourceLoop.Stop();
-		StartCoroutine(AudioLoop(sound, volume));
-	}
+        StartCoroutine(AudioLoop(sound, volume));
+    }
+
+    public void PlaySoundOneShot(AudioClip sound, float volume)
+    {
+        audioSource.PlayOneShot(sound, volume);
+    }
 
     public void UpdateHealthBar()
     {
@@ -82,84 +81,84 @@ public class Stats : MonoBehaviour
     }
 
     public void TakeDamage(int damage)
-	{
-		PlaySoundOneShot(takeDamage, 1);
-		currentHp -= damage;
-		UpdateHealthBar();
+    {
+        PlaySoundOneShot(takeDamage[Random.Range(0, 2)], 1);
+        currentHp -= damage;
+        UpdateHealthBar();
     }
 
-	private void OnEnable()
-	{
-		ExpManager.Instance.ExpChange += HandleExpChange;
-	}
+    private void OnEnable()
+    {
+        ExpManager.Instance.ExpChange += HandleExpChange;
+    }
 
-	private void OnDisable()
-	{
-		ExpManager.Instance.ExpChange -= HandleExpChange;
-	}
+    private void OnDisable()
+    {
+        ExpManager.Instance.ExpChange -= HandleExpChange;
+    }
 
-	public void HandleExpChange(int newExp)
-	{
-		currentExp += newExp;
+    public void HandleExpChange(int newExp)
+    {
+        currentExp += newExp;
 
-		expBar.SetExp(currentExp);
-		expBar.MaxExp(maxExp);
+        expBar.SetExp(currentExp);
+        expBar.MaxExp(maxExp);
 
-		if (currentExp >= maxExp)
-		{
-			LevelUp();
-		}
-	}
+        if (currentExp >= maxExp)
+        {
+            LevelUp();
+        }
+    }
 
-	private void LevelUp()
-	{
-		PlaySoundOneShot(lvlUp, 1);
+    private void LevelUp()
+    {
+        PlaySoundOneShot(lvlUp, 1);
 
-		//Slash.heal++;
-		EnemyScript.enemyDmg++;
+        //Slash.heal++;
+        EnemyScript.enemyDmg++;
 
-		currentLevel++;
+        currentLevel++;
 
         maxExp += 100;
         currentExp = 0;
-		levelCounter.LevelCount(currentLevel);
-		expBar.SetStartExp(currentExp);
+        levelCounter.LevelCount(currentLevel);
+        expBar.SetStartExp(currentExp);
 
         maxHp += 10;
         currentHp = maxHp;
-		UpdateHealthBar();
+        UpdateHealthBar();
 
         if (currentLevel % 3 == 0)
-		{
-			shopUIManager.UnlockShop();
+        {
+            shopUIManager.UnlockShop();
             PlaySoundOneShot(magnetUnlocked, 1);
 
             if (EnemySpawn.spawnRate > 1f)
-			{
-				EnemySpawn.spawnRate -= 1f;
-			}
-			else if (EnemySpawn.spawnRate > 0.5f) //wrogowie spawnia sie czybko ale skala jest liniowa
-			{
-				EnemySpawn.spawnRate -= 0.1f;
-			}
-		}
+            {
+                EnemySpawn.spawnRate -= 1f;
+            }
+            else if (EnemySpawn.spawnRate > 0.5f) //wrogowie spawnia sie szybko ale skala jest liniowa
+            {
+                EnemySpawn.spawnRate -= 0.1f;
+            }
+        }
 
-		if (currentLevel == 5)
+        if (currentLevel == 5)
         {
             MagnetCollider.SetActive(true);
-			StartCoroutine(magnetText.Text());
-		}
-	}
+            StartCoroutine(magnetText.Text());
+        }
+    }
 
-	public void GainExp()
-	{
-		if (exp >= currentExp)
-		{
-			currentExp = exp;
-		}
-	}
+    public void GainExp()
+    {
+        if (exp >= currentExp)
+        {
+            currentExp = exp;
+        }
+    }
 
-	/* leczenie atakiem i tak na razie jest wy³¹czone
+    /* leczenie atakiem i tak na razie jest wy³¹czone
 	 * private void OverHeal()
 	{
 		if (currentHp > maxHp * 2)
@@ -181,18 +180,26 @@ public class Stats : MonoBehaviour
 		}
 	}*/
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		//overHeal = 0;
-		currentHp = maxHp;
-		healthBar.SetMaxHealth(maxHp);
+    private void Reset()
+    {
+        currentExp = 1;
+        currentLevel = 1;
+        maxHp = 20;
+        maxExp = 100;
+        EnemyScript.enemyDmg = 5;
+    }
 
-		currentExp = exp;
-		expBar.SetStartExp(currentExp);
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset();
 
-		currentExp = 1;
+        //overHeal = 0;
+        currentHp = maxHp;
+        healthBar.SetMaxHealth(maxHp);
 
-		currentLevel = 1;
-	} //usunąłem cały update i wrzuciłem funkcje do kodu tam gdzie są wywoływane np. udpate healthbar
+        currentExp = exp;
+        expBar.SetStartExp(currentExp);
+
+    } //usunąłem cały update i wrzuciłem funkcje do kodu tam gdzie są wywoływane np. udpate healthbar
 }
